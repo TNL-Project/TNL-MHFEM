25506a11c63a2d211b0598c63c4d0c902d093470
Jakub Klinkovský <klinkjak@fjfi.cvut.cz>
2016-12-29 16:41:59 +0100

    coefficients for triangles

--- include/TNL/MHFEM/MassMatrix.h
+++ include/TNL/MHFEM/MassMatrix.h
@@ -4,6 +4,8 @@
 #include <TNL/Meshes/MeshEntity.h>
 
 #include "../lib_general/mesh_helpers.h"
+#include "../lib_general/StaticMatrix.h"
+#include "../lib_general/GE.h"
 
 namespace mhfem
 {
@@ -470,6 +472,7 @@ class MassMatrix< TNL::Meshes::MeshEntity< MeshConfig, TNL::Meshes::MeshEdgeTopo
 {
 public:
     using MeshEntity = TNL::Meshes::MeshEntity< MeshConfig, TNL::Meshes::MeshEdgeTopology >;
+    using LocalIndex = typename MeshEntity::LocalIndexType;
     static constexpr MassLumping lumping = MassLumping::disabled;
 
     // number of independent values defining the matrix
@@ -481,9 +484,11 @@ public:
     update( const Mesh & mesh,
             const MeshEntity & entity,
             MeshDependentData & mdd,
-            const int & i,
-            const int & j )
+            const LocalIndex i,
+            const LocalIndex j )
     {
+        static_assert( std::is_same< typename Mesh::Config, MeshConfig >::value, "wrong mesh" );
+
         const auto K = entity.getIndex();
         typename MeshDependentData::RealType* storage = mdd.b_ijK( i, j, K );
         const auto h_x = getEntityMeasure( mesh, entity );
@@ -494,11 +499,11 @@ public:
     __cuda_callable__
     static inline typename MeshDependentData::RealType
     b_ijKef( const MeshDependentData & mdd,
-             const int & i,
-             const int & j,
+             const LocalIndex i,
+             const LocalIndex j,
              const typename MeshDependentData::IndexType & K,
-             const int & e,
-             const int & f )
+             const LocalIndex e,
+             const LocalIndex f )
     {
         TNL_ASSERT( e < FacesPerCell< MeshEntity >::value && f < FacesPerCell< MeshEntity >::value, );
 
@@ -506,6 +511,118 @@ public:
         if( e == f )
             return 2 * storage[ 0 ];
         return storage[ 0 ];
+    }
+
+    template< typename MeshDependentData >
+    __cuda_callable__
+    static inline typename MeshDependentData::RealType
+    b_ijKe( const MeshDependentData & mdd,
+            const LocalIndex i,
+            const LocalIndex j,
+            const typename MeshDependentData::IndexType & K,
+            const LocalIndex e )
+    {
+        TNL_ASSERT( e < FacesPerCell< MeshEntity >::value, );
+
+        const typename MeshDependentData::RealType* storage = mdd.b_ijK( i, j, K );
+        return 3 * storage[ 0 ];
+    }
+};
+
+template< typename MeshConfig >
+class MassMatrix< TNL::Meshes::MeshEntity< MeshConfig, TNL::Meshes::MeshTriangleTopology >, MassLumping::disabled >
+{
+public:
+    using MeshEntity = TNL::Meshes::MeshEntity< MeshConfig, TNL::Meshes::MeshTriangleTopology >;
+    using LocalIndex = typename MeshEntity::LocalIndexType;
+    static constexpr MassLumping lumping = MassLumping::disabled;
+
+    // number of independent values defining the matrix
+    static constexpr int size = 9;
+
+    template< typename Mesh, typename MeshDependentData >
+    __cuda_callable__
+    static inline void
+    update( const Mesh & mesh,
+            const MeshEntity & entity,
+            MeshDependentData & mdd,
+            const LocalIndex i,
+            const LocalIndex j )
+    {
+        static_assert( std::is_same< typename Mesh::Config, MeshConfig >::value, "wrong mesh" );
+
+        StaticMatrix< 3, 6, typename Mesh::RealType > matrix;
+
+        const auto& v0 = mesh.template getEntity< 0 >( entity.template getSubentityIndex< 0 >( 0 ) );
+        const auto& v1 = mesh.template getEntity< 0 >( entity.template getSubentityIndex< 0 >( 1 ) );
+        const auto& v2 = mesh.template getEntity< 0 >( entity.template getSubentityIndex< 0 >( 2 ) );
+
+        const auto P0 = v0.getPoint() - v2.getPoint();
+        const auto P1 = v1.getPoint() - v2.getPoint();
+        // P2 = 0
+        
+        const auto P00 = P0 * P0;
+        const auto P01 = P0 * P1;
+        const auto P11 = P1 * P1;
+
+        const auto K = entity.getIndex();
+        const auto denominator = 24 * mdd.D_ijK( i, j, K ) * getEntityMeasure( mesh, entity );
+
+        matrix.setElementFast( 0, 0,  ( 3 * P00 + P11 - 3 * P01 ) / denominator );
+        matrix.setElementFast( 1, 1,  ( P00 + 3 * P11 - 3 * P01 ) / denominator );
+        matrix.setElementFast( 2, 2,  ( P00 + P11 + P01 ) / denominator );
+        matrix.setElementFast( 0, 1,  ( 3 * P01 - P00 - P11 ) / denominator );
+        matrix.setElementFast( 0, 2,  ( P11 - P00 - P01 ) / denominator );
+        matrix.setElementFast( 1, 2,  ( P00 - P11 - P01 ) / denominator );
+
+        matrix.setElementFast( 1, 0,  matrix.getElementFast( 0, 1 ) );
+        matrix.setElementFast( 2, 0,  matrix.getElementFast( 0, 2 ) );
+        matrix.setElementFast( 2, 1,  matrix.getElementFast( 1, 2 ) );
+
+        // set identity in the right half
+        for( LocalIndex i = 0; i < 3; i++ )
+            for( LocalIndex j = 0; j < 3; j++ )
+                if( i == j )
+                    matrix.setElementFast( i, j + 3, 1.0 );
+                else
+                    matrix.setElementFast( i, j + 3, 0.0 );
+
+        // invert
+        GE( matrix );
+
+        typename MeshDependentData::RealType* storage = mdd.b_ijK( i, j, K );
+        // store the inverse in the packed format (upper triangle, column by column)
+        // see: http://www.netlib.org/lapack/lug/node123.html
+        storage[ 0 ] = matrix.getElementFast( 0, 0 );
+        storage[ 1 ] = matrix.getElementFast( 0, 1 );
+        storage[ 2 ] = matrix.getElementFast( 1, 1 );
+        storage[ 3 ] = matrix.getElementFast( 0, 2 );
+        storage[ 4 ] = matrix.getElementFast( 1, 2 );
+        storage[ 5 ] = matrix.getElementFast( 2, 2 );
+
+        // the last 3 values are the sums for the b_ijK coefficients
+        storage[ 6 ] = matrix.getElementFast( 0, 0 ) + matrix.getElementFast( 0, 1 ) + matrix.getElementFast( 0, 2 );
+        storage[ 7 ] = matrix.getElementFast( 0, 1 ) + matrix.getElementFast( 1, 1 ) + matrix.getElementFast( 1, 2 );
+        storage[ 8 ] = matrix.getElementFast( 0, 2 ) + matrix.getElementFast( 1, 2 ) + matrix.getElementFast( 2, 2 );
+    }
+
+    template< typename MeshDependentData >
+    __cuda_callable__
+    static inline typename MeshDependentData::RealType
+    b_ijKef( const MeshDependentData & mdd,
+             const LocalIndex i,
+             const LocalIndex j,
+             const typename MeshDependentData::IndexType & K,
+             LocalIndex e,
+             LocalIndex f )
+    {
+        TNL_ASSERT( e < FacesPerCell< MeshEntity >::value && f < FacesPerCell< MeshEntity >::value, );
+
+        if( e > f )
+            TNL::swap( e, f );
+
+        const typename MeshDependentData::RealType* storage = mdd.b_ijK( i, j, K );
+        return storage[ e + ( f * (f+1) ) / 2 ];
     }
 
     template< typename MeshDependentData >
@@ -520,7 +637,7 @@ public:
         TNL_ASSERT( e < FacesPerCell< MeshEntity >::value, );
 
         const typename MeshDependentData::RealType* storage = mdd.b_ijK( i, j, K );
-        return 3 * storage[ 0 ];
+        return storage[ 6 + e ];
     }
 };
 
