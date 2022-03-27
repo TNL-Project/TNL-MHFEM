66d4b22e8f648f471fc0bd2dc527d97e6e2e8a79
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2022-03-27 14:26:34 +0200

    update LBM and TNL submodules, refactor MPI communicators and make simWT1 optimize the ordering of ranks

--- include/TNL/MHFEM/control.h
+++ include/TNL/MHFEM/control.h
@@ -31,12 +31,13 @@ void distributeFaces( DistributedMesh& mesh )
 template< typename Problem >
 void init( Problem& problem,
            typename Problem::DistributedHostMeshPointer& meshPointer,
-           const TNL::Config::ParameterContainer& parameters )
+           const TNL::Config::ParameterContainer& parameters,
+           const TNL::MPI::Comm& communicator = MPI_COMM_WORLD )
 {
     const TNL::String meshFile = parameters.getParameter< TNL::String >( "mesh" );
     const TNL::String meshFileFormat = parameters.getParameter< TNL::String >( "mesh-format" );
 #ifdef HAVE_MPI
-    if( ! TNL::Meshes::loadDistributedMesh( *meshPointer, meshFile, meshFileFormat ) )
+    if( ! TNL::Meshes::loadDistributedMesh( *meshPointer, meshFile, meshFileFormat, communicator ) )
         throw std::runtime_error( "failed to load the distributed mesh from file " + meshFile );
 
     if( meshPointer->getCommunicator() != MPI_COMM_NULL )
@@ -45,7 +46,7 @@ void init( Problem& problem,
 #else
     if( ! TNL::Meshes::loadMesh( meshPointer->getLocalMesh(), meshFile, meshFileFormat ) )
         throw std::runtime_error( "failed to load the mesh from file " + meshFile );
-    meshPointer->setCommunicator( MPI_COMM_WORLD );
+    meshPointer->setCommunicator( communicator );
 #endif
 
     // estimate memory before further allocations
