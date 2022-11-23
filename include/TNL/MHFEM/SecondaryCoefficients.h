94b853861fd2edf0d16d360ff414d29cda6f0373
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2022-11-23 23:01:04 +0100

    mhfem: removed useless specializations for MassLumpingDependentCoefficients - C++17 has if constexpr

--- include/TNL/MHFEM/SecondaryCoefficients.h
+++ include/TNL/MHFEM/SecondaryCoefficients.h
@@ -1,17 +1,50 @@
 #pragma once
 
-#include "MassLumpingDependentCoefficients.h"
+#include "MassMatrix.h"
 
 namespace mhfem {
 
 template< typename MeshDependentData >
 struct SecondaryCoefficients
-    : public MassLumpingDependentCoefficients< MeshDependentData >
 {
     using RealType = typename MeshDependentData::RealType;
     using IndexType = typename MeshDependentData::IndexType;
     using MassMatrix = typename MeshDependentData::MassMatrix;
     using MeshType = typename MeshDependentData::MeshType;
+
+    template< typename FaceVectorType >
+    __cuda_callable__
+    static inline RealType
+    v_iKE( const MeshDependentData & mdd,
+           const FaceVectorType & faceIndexes,
+           const int i,
+           const IndexType K,
+           const IndexType E,
+           const int e )
+    {
+        static_assert( FaceVectorType::getSize() == MeshDependentData::FacesPerCell );
+
+        // split into 2 sums to limit catastrophic truncation
+        RealType sum_K = 0;
+        RealType sum_E = 0;
+
+        for( int j = 0; j < MeshDependentData::NumberOfEquations; j++ ) {
+            const auto b = MassMatrix::b_ijKe( mdd, i, j, K, e );
+            sum_K += b * mdd.Z_iK( j, K );
+            // the sum of b_ijKef * Z_iF over f gets trivial when the mass matrix is diagonal
+            if constexpr( MeshDependentData::MassMatrix::is_diagonal ) {
+                sum_E += b * mdd.Z_iF( j, E );
+            }
+            else {
+                for( int f = 0; f < MeshDependentData::FacesPerCell; f++ ) {
+                    const IndexType & F = faceIndexes[ f ];
+                    sum_E += MassMatrix::b_ijKef( mdd, i, j, K, e, f ) * mdd.Z_iF( j, F );
+                }
+            }
+        }
+
+        return sum_K - sum_E + mdd.w_iKe( i, K, e );
+    }
 
     __cuda_callable__
     static RealType
@@ -35,6 +68,33 @@ struct SecondaryCoefficients
             sum += MassMatrix::b_ijKe( mdd, i, xxx, K, e ) * mdd.R_ijKe( xxx, j, K, f );
         }
         return MassMatrix::b_ijKef( mdd, i, j, K, e, f ) - sum;
+    }
+
+    template< typename FaceVectorType >
+    __cuda_callable__
+    static inline RealType
+    R_ijKe( const MeshDependentData & mdd,
+            const FaceVectorType & faceIndexes,
+            const int i,
+            const int j,
+            const IndexType K,
+            const int e,
+            const RealType tau )
+    {
+        static_assert( FaceVectorType::getSize() == MeshDependentData::FacesPerCell );
+
+        if constexpr( MeshDependentData::MassMatrix::is_diagonal ) {
+            const IndexType & E = faceIndexes[ e ];
+            return mdd.m_iE_upw( i, E ) * MassMatrix::b_ijKe( mdd, i, j, K, e ) * tau;
+        }
+        else {
+            RealType R = 0;
+            for( int f = 0; f < MeshDependentData::FacesPerCell; f++ ) {
+                const IndexType & F = faceIndexes[ f ];
+                R += mdd.m_iE_upw( i, F ) * MassMatrix::b_ijKef( mdd, i, j, K, f, e );
+            }
+            return R * tau;
+        }
     }
 
     template< typename FaceVectorType >
